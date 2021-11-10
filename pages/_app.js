06efe6a3ddb6b8c6e28ec517beb087e77b2eac46
oskvr <oskar.puustinen@gmail.com>
2021-11-10 10:51:46 +0100

    Material setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  // Custom theme props
+});
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Course Registration App</title>
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
