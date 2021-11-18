2107c9b0ac3b81e894206d8f6cc834c19ef93298
erik torkelsson <erik.torkelsson@hotmail.com>
2021-11-18 19:48:28 +0100

    Tillfällig token check

--- pages/account/login/index.js
+++ pages/account/login/index.js
@@ -1,111 +1,51 @@
-import {useState} from 'react';
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Link from "../../../components/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import LoginForm from '../../../components/LoginForm';
+import { Button, Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
-export default function login() {
+export default function index() {
 
-  const [email,setEmail] = useState('');
-  const [passWord, setPassWord] = useState('');
+    // Får göra en seriösare check när jag vet hur vi ska förvara token
 
-  const requestBody = {
-    "email": email,
-    "password": passWord
-  };
+    let token = "";
 
-  const onEmailChanged = (e) => setEmail(e.target.value);
-  const onPassWordChanged = (e) => setPassWord(e.target.value);
+    if (typeof window !== "undefined") {
+      token = window.localStorage.getItem('token')    
+    }
 
-  const handleSubmit = async (event) => {
+    const onLogoutClicked = () => {
+        window.localStorage.setItem('token', "");
+    }
 
-    const res = await fetch('https://localhost:44314/api/auth/login', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    });
-
-    const data = await res.text();
-
-    console.log(res);
-
-    console.log(data);
-
-    localStorage.setItem('token', data);
-
-    event.preventDefault();
-    
-  };
-
-  return (
-    <Container
-      maxWidth="xs"
-      sx={{ minHeight: "70vh", display: "grid", placeItems: "center" }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Logga in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            value={email}
-            onChange={onEmailChanged}
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            value={passWord}
-            onChange={onPassWordChanged}
-            name="password"
-            label="Lösenord"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="Button"
-            fullWidth
-            variant="contained"
-            onClick={handleSubmit}
-            sx={{ mt: 3, mb: 2 }}
+    if(token){
+        return(
+          <Container
+            sx={{
+              display: "grid",
+              justifyContent: "center",
+              paddingTop: 15,
+            }}
           >
-            Logga in
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link href="/account/register" variant="body2">
-                {"Har du inget konto? Registrera"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
-  );
+            <Box
+              component="form"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h3" fontWeight="light" textAlign="center" marginBottom="15px">
+                Du är inloggad
+              </Typography>
+              <Button type="submit" variant="contained" onClick={onLogoutClicked} >
+                Logga ut
+              </Button>
+            </Box>
+          </Container>
+        )
+    }
+    else{
+        return(
+            <LoginForm></LoginForm>
+        )        
+    }
 }
