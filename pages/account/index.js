3e31a0ec55b46061ccbeef0b4425ae7da861e613
erik torkelsson <erik.torkelsson@hotmail.com>
2021-11-24 15:57:33 +0100

    Ui grund för mitt krypin(Behöver lite finputs och fetch)

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,8 +1,19 @@
-import { Button, Container, Grid, Typography } from "@mui/material";
+import { Button, Container, Grid, ListItemSecondaryAction, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import Image from "next/image";
 import Link from "@/components/Link";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Mock from '../../mock/mockCourses'
+
+
 export default function Home() {
+
   return (
     <Container
       sx={{
@@ -11,6 +22,27 @@ export default function Home() {
         paddingTop: 15,
       }}
     >
+    <Typography variant="h3" fontWeight="light" textAlign="start" marginBottom="20px">Personuppgifter:</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "start",
+        marginBottom: "20px",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+          marginRight: "20px",
+        }}
+      >
+        <Typography variant="h5" fontWeight="light" textAlign="start">Förnamn</Typography>
+        <Typography variant="h5" fontWeight="light" textAlign="start">Efternamn</Typography>
+        <Typography variant="h5" fontWeight="light" textAlign="start">Email</Typography>
+      </Box>
       <Box
         sx={{
           display: "flex",
@@ -18,10 +50,37 @@ export default function Home() {
           justifyContent: "center",
         }}
       >
-        <Typography variant="h3" fontWeight="light" textAlign="center">
-          Min profil
-        </Typography>
+        <Typography variant="h5" fontWeight="light" textAlign="start">Erik</Typography>
+        <Typography variant="h5" fontWeight="light" textAlign="start">Torkelsson</Typography>
+        <Typography variant="h5" fontWeight="light" textAlign="start">erik.torkelsson@student.kyh.se</Typography>
       </Box>
+    </Box>
+      <Typography variant="h3" fontWeight="light" textAlign="start">Bokade kurser:</Typography>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        {Mock.map((course, i) => (
+          <ListItem key={course.CourseId} alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src={course.ImgSrc} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={course.Subject}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  {"Från: " + course.StartDate + " till: " + course.EndDate}
+                </Typography>
+              </React.Fragment>
+            }
+          />
+          <Button>Avboka</Button>
+        </ListItem>
+        ))}   
+      </List>
     </Container>
   );
 }
